177dc0d71bf74bec890bc712498b03bbe59c9405
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-05-26 22:45:33 +0300

    fix

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "mergele12",
-    host: "localhost",
-    port: 5432,
-    database: "tododb"
+    client: 'pg',
+    connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+    }
 });
 
 module.exports = pool;
